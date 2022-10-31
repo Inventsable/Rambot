2606d8427002334ae45ac0fd184dfb7d6c6aaa39
Tom Scharstein <trscharstein@gmail.com>
2022-10-30 19:24:26 -0700

    Init

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const { Client, MessageAttachment } = require("discord.js");
 const client = new Client();
 
 client.on("message", (message) => {
+  /**
+   * To see what the message has available, try logging it to console:
+   */
+  // console.log(message);
+
+  if (message.author.bot) return null; // We don't want our bot to care about / interfere with other bots
+
   if (/rambot/i.test(message.content)) {
     message.react("👀");
+  }
+  if (/^ping$/i.test(message.content)) {
+    message.reply("pong");
   }
 });
 
